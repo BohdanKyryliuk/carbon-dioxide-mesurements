2cfac4997adddd112efae199d7392383509e9626
Bohdan Kyryliuk <bkyryliuk@codifire.com>
2022-05-14 19:11:11 +0300

    Added models and migrations: `Sensor`, `Status`, `Alert`

---
+++ database/migrations/2022_05_14_155242_create_statuses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\Status;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sensor_id')->constrained()->restrictOnDelete()->cascadeOnUpdate();
+            $table->enum('name', [Status::OK->value, Status::WARN->value, Status::ALERT->value]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+};
