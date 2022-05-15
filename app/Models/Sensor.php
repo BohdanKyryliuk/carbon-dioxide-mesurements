ec283adf3f2c92a2cba83b722c4a94fc45f59cfe
Bohdan Kyryliuk <bkyryliuk@codifire.com>
2022-05-15 11:19:31 +0300

    Added package `"dyrynda/laravel-model-uuid"`

--- app/Models/Sensor.php
+++ app/Models/Sensor.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\BindsOnUuid;
+use Dyrynda\Database\Support\GeneratesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Sensor extends Model
 {
     use HasFactory;
+    use BindsOnUuid;
+    use GeneratesUuid;
+
+    protected $fillable = [
+        'uuid',
+    ];
 }
