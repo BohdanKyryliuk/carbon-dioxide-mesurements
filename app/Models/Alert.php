b0016f38e8c91d136ab5beacbc588dc7167a51a4
Bohdan Kyryliuk <bkyryliuk@codifire.com>
2022-05-15 11:24:51 +0300

    Added relationships

--- app/Models/Alert.php
+++ app/Models/Alert.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasRelationships;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Alert extends Model
 {
     use HasFactory;
+    use HasRelationships;
+
+    public function sensor(): BelongsTo
+    {
+        return $this->belongsTo(Sensor::class);
+    }
 }
--- app/Models/Sensor.php
+++ app/Models/Sensor.php
@@ -4,16 +4,30 @@ namespace App\Models;
 
 use Dyrynda\Database\Support\BindsOnUuid;
 use Dyrynda\Database\Support\GeneratesUuid;
+use Illuminate\Database\Eloquent\Concerns\HasRelationships;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Sensor extends Model
 {
     use HasFactory;
     use BindsOnUuid;
     use GeneratesUuid;
+    use HasRelationships;
 
     protected $fillable = [
         'uuid',
     ];
+
+    public function status(): HasOne
+    {
+        return $this->hasOne(Status::class);
+    }
+
+    public function alert(): HasMany
+    {
+        return $this->hasMany(Alert::class);
+    }
 }
