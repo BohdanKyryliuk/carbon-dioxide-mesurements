2104f44ef40c21d28a03f8ceaf445dd6e9529fdd
Bohdan Kyryliuk <bkyryliuk@codifire.com>
2022-05-15 12:51:22 +0300

    Added `MesurementsController` and storing route

---
+++ app/Http/Api/V1/Requests/MesurementsStoreRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Api\V1\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MesurementsStoreRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'co2' => ['required', 'integer', 'between:350,10000'],
+            'time' => ['required', 'date_format:Y-m-d\TH:i:sP']
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Api\V1\Controllers\MesurementsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::name('api.')
+    ->prefix('v1')
+    ->group(function () {
+        Route::post('/sensors/{uuid}/mesurements', [MesurementsController::class, 'store'])
+            ->whereUuid('uuid')
+            ->name('mesurements');
+    });
