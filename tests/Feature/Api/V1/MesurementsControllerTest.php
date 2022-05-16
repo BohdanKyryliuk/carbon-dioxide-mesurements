8442c39e6a6134d57234a3ec53e95cb60174d7fb
Bohdan Kyryliuk <bkyryliuk@codifire.com>
2022-05-16 10:14:22 +0300

    Updated test `MeasurementsControllerTest`

--- tests/Feature/Api/V1/MesurementsControllerTest.php
+++ tests/Feature/Api/V1/MesurementsControllerTest.php
@@ -1,14 +1,22 @@
 <?php
 
+use App\Services\Actions\StoreMeasurementsAction;
+use Illuminate\Support\Facades\Queue;
+use Spatie\QueueableAction\Testing\QueueableActionFake;
+
 it('saves on store', function () {
     $co2 = $this->faker()->numberBetween(350, 10000);
     $time = $this->faker()->date('Y-m-d\TH:i:sP');
     $uuid = $this->faker()->uuid();
 
+    Queue::fake();
+
     $response = $this->post(route('api.measurements', [$uuid]), [
         'co2' => $co2,
         'time' => $time,
     ]);
+
+    QueueableActionFake::assertPushed(StoreMeasurementsAction::class);
 
     $response->assertCreated();
 });
